a55bdd130ef1852dae4bc0e204af162e275f5dc8
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-07-16 20:51:40 -0400

    Replace tab

--- container.lua
+++ container.lua
@@ -116,7 +116,7 @@ area_containers.container = {}
 area_containers.all_container_states = {}
 local all_container_variants = {
 	"off", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
-	"1000",	"1001", "1010", "1011", "1100", "1101", "1110", "on",
+	"1000", "1001", "1010", "1011", "1100", "1101", "1110", "on",
 }
 for i, variant in ipairs(all_container_variants) do
 	area_containers.all_container_states[i] =
