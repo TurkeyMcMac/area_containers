580e36edc7838941e409ac9e70d2a5c7f2c52580
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-07-19 19:50:33 -0400

    Remove a space from the license notice indentation
    This makes the indentation consistent with that of other comments.

--- container.lua
+++ container.lua
@@ -1,20 +1,20 @@
 --[[
-    Copyright (C) 2021  Jude Melton-Houghton
+   Copyright (C) 2021  Jude Melton-Houghton
 
-    This file is part of area_containers. It implements node functionality.
+   This file is part of area_containers. It implements node functionality.
 
-    area_containers is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   area_containers is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    area_containers is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+   area_containers is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with area_containers. If not, see <https://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU Lesser General Public License
+   along with area_containers. If not, see <https://www.gnu.org/licenses/>.
 ]]
 
 --[[
