045111d9b7736a31087a5024c9958772c56e817d
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-07-18 21:41:51 -0400

    Space out containers more by default
    This way, entering one container will probably only make active that
    container's inside.

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ area_containers.settings = {
 	y_level_blocks = tonumber(minetest.settings:get(
 		"area_containers_y_level_blocks") or 1931),
 	spacing_blocks = tonumber(minetest.settings:get(
-		"area_containers_spacing_blocks") or 2),
+		"area_containers_spacing_blocks") or 15),
 	enable_crafts = minetest.settings:get_bool(
 		"area_containers_enable_crafts", true),
 }
