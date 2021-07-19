a9109badbaef46f1a97631632501fb781d0e7684
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-07-19 08:53:16 -0400

    Make craft registration safer

--- crafts.lua
+++ crafts.lua
@@ -19,26 +19,27 @@
 
 
 function area_containers.register_crafts()
-	-- I think that crafts silently don't register if the ingredients are
-	-- not themselves registered?
-	minetest.register_craft({
-		output = "area_containers:container",
-		recipe = {
-			{
-				"default:steel_ingot",
-				"default:steel_ingot",
-				"default:steel_ingot"
+	if minetest.registered_craftitems["default:steel_ingot"] and
+	   minetest.registered_nodes["default:mese"] then
+		minetest.register_craft({
+			output = "area_containers:container",
+			recipe = {
+				{
+					"default:steel_ingot",
+					"default:steel_ingot",
+					"default:steel_ingot"
+				},
+				{
+					"default:steel_ingot",
+					"default:mese",
+					"default:steel_ingot",
+				},
+				{
+					"default:steel_ingot",
+					"default:steel_ingot",
+					"default:steel_ingot",
+				},
 			},
-			{
-				"default:steel_ingot",
-				"default:mese",
-				"default:steel_ingot",
-			},
-			{
-				"default:steel_ingot",
-				"default:steel_ingot",
-				"default:steel_ingot",
-			},
-		},
-	})
+		})
+	end
 end
